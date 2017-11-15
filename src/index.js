b401670581b87dc1be9b4c9d24f97b7a3f77af39
Alexandra <nanshee4@gmail.com>
2017-11-15 17:29:24 +0100

    tuto udemy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
+//CSS
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+//JS perso
+import { sampleText} from "./sampleText.js";
+
+class App extends React.Component{
+    render() {
+        return(
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-sm-6">
+                        <textarea value={sampleText} rows="35" className="form-control">
+
+                        </textarea>
+                    </div>
+
+                    <div className="col-sm-6">
+                        {sampleText}
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+render(
+    <App/>,
+    document.getElementById('root')
+);
