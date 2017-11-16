acff8fa10d01cabf7a90814d37830a4a58bd0af5
Alexandra <nanshee4@gmail.com>
2017-11-16 14:39:40 +0100

    fin du tuto markdow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//import des différents composant de l'app
 import React from 'react';
 import { render } from 'react-dom';
 //CSS
@@ -7,21 +8,59 @@ import './index.css';
 //JS perso
 import { sampleText} from "./sampleText.js";
 
+//marked.js
+import marked from 'marked';
+
+
 class App extends React.Component{
+
+    state = {
+        text: sampleText
+    };
+
+    //permet de récupérer ce qui a été mis en local storage
+    componentWillMount(){
+        const text = localStorage.getItem('text');
+
+        if (text){
+            this.setState({ text });
+        }
+    }
+
+    //créer la mise a jour du component, créer le local storage
+    componentWillUpdate(nextProps, nextState){
+        localStorage.setItem('text', nextState.text);
+    }
+
+    //fonction perso
+    editText = (event) => {
+        const text = event.target.value;
+        this.setState({ text });
+    };
+
+    renderText = (text) =>{
+        const renderText = marked(text, {sanitize: true});
+        return { __html: renderText };
+    };
+
+    //rendu de l'app
     render() {
         return(
             <div className="container">
                 <div className="row">
 
                     <div className="col-sm-6">
-                        <textarea value={sampleText} rows="35" className="form-control">
+                        <textarea
+                            value={this.state.text}
+                            rows="35"
+                            className="form-control"
+                            onChange={(e) =>this.editText(e)}
+                        >
 
                         </textarea>
                     </div>
 
-                    <div className="col-sm-6">
-                        {sampleText}
-                    </div>
+                    <div dangerouslySetInnerHTML={this.renderText(this.state.text)}/>
 
                 </div>
             </div>
